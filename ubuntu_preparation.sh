a59b785f8c6a36d6ab23e92c84903f906411d741
taquocviet <quocvietbkhn@gmail.com>
2019-10-05 20:43:15 +0700

    Update ubuntu_preparation.sh
    Add arp-scan

--- ubuntu_preparation.sh
+++ ubuntu_preparation.sh
@@ -4,7 +4,7 @@ apt-get upgrade -y
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 apt-get update
-apt-get install -y vim sublime-text build-essential ibus-unikey openssh-server vlc git meld
+apt-get install -y vim sublime-text build-essential ibus-unikey openssh-server vlc git meld arp-scan
 cd ~/Downloads/
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
