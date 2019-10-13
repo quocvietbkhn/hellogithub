8d69e61a3316c93ba1fa98e79eff41b5ae2f25a3
taquocviet <quocvietbkhn@gmail.com>
2019-10-13 14:41:18 +0700

    Update ubuntu_preparation.sh

--- ubuntu_preparation.sh
+++ ubuntu_preparation.sh
@@ -4,7 +4,7 @@ apt-get upgrade -y
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 apt-get update
-apt-get install -y vim sublime-text build-essential ibus-unikey openssh-server vlc git meld arp-scan
+apt-get install -y vim sublime-text build-essential ibus-unikey openssh-server vlc git meld arp-scan exfat-fuse exfat-utils
 cd ~/Downloads/
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
